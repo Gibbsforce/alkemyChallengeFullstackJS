c8b6ac324c3b6d25b765bcce3a33bbc4b4c0c839
Alvaro Avalos <tlalvaro15@gmail.com>
2022-05-26 23:29:04 -0500

    preparing the backend for be consumed by the frontend

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 import { dotenv } from "./config.js"
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
 import router from "./routers/index.js"
 import { PORT } from "./utils/globalConstants.js"
 const app = express()
+app.use(cors())
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", router)
+app.get("*", (req, res) => res.status(404).json({ message: "Not Found" }))
 app.listen(PORT, () =>
   console.log(`Listening on port: ${PORT}, url: http://localhost:${PORT}`)
 )
